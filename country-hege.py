fb0e972f0611e8795059cfa04f62830f34eeb9c2
romain.fontugne <romain.fontugne@gmail.com>
2019-05-16 15:33:14 +0900

    revert the behavior of eyeball option

--- country-hege.py
+++ country-hege.py
@@ -15,25 +15,7 @@ def get_pop_estimate(cc, min_population):
 
     return {x['as']:x for x in pop_est}
 
-
-if __name__ == '__main__':
-
-    parser = argparse.ArgumentParser()
-    parser.add_argument('country_code', type=str,
-                    help='Country code')
-    parser.add_argument('-e', '--eyeball', action='count',
-                        help='count eyeball asn')
-    parser.add_argument('-n', '--noweight', action='count',
-                        help="don't weight by eyeball population")
-    parser.add_argument('-t', '--top', type=int, default=10,
-                        help="print top ASN")
-    parser.add_argument('-m', '--min_population', type=float, default=0.01,
-                        help="print top ASN")
-    args = parser.parse_args()
-
-    pop_est = get_pop_estimate(args.country_code, args.min_population)
-    print('Found {} eyeball networks in {}'.format(len(pop_est), args.country_code))
-
+def compute_hegemony(pop_est, args):
     hege = Hegemony(originasns=pop_est.keys(), start="2019-05-15 00:00", end="2019-05-15 00:01")
     results = defaultdict(float)
     weight_sum = 0
@@ -46,7 +28,7 @@ if __name__ == '__main__':
             originasn = hege['originasn']
 
             # don't count originasn (eyeball network)
-            if not args.eyeball and asn==originasn:
+            if args.remove_eyeball and asn==originasn:
                 continue
 
             if not args.noweight:
@@ -56,7 +38,28 @@ if __name__ == '__main__':
             weight_sum += w
 
     results = {asn:w/weight_sum for asn, w in results.items()}
-    sorted_results = sorted(results.items(), key=lambda kv: kv[1])
+    return sorted(results.items(), key=lambda kv: kv[1])
+
+if __name__ == '__main__':
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument('country_code', type=str,
+                    help='Country code')
+    parser.add_argument('-r', '--remove_eyeball', action='count',
+                        help="don't count eyeball ASes")
+    parser.add_argument('-n', '--noweight', action='count',
+                        help="don't weight by eyeball population")
+    parser.add_argument('-t', '--top', type=int, default=10,
+                        help="print top ASN")
+    parser.add_argument('-m', '--min_population', type=float, default=0.01,
+                        help="print top ASN")
+    args = parser.parse_args()
+
+    pop_est = get_pop_estimate(args.country_code, args.min_population)
+    print('Found {} eyeball networks in {}'.format(len(pop_est), args.country_code))
+
+    sorted_results = compute_hegemony(pop_est,args)
+
     for i in range(1,min(len(sorted_results),args.top+1)):
         asn, val = sorted_results[len(sorted_results)-i]
         label = '-'
