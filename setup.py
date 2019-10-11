7c03296dcb8238dd02b26615435782164915020b
romain.fontugne <romain.fontugne@gmail.com>
2019-10-11 16:22:49 +0900

    'add

---
+++ setup.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+
+from setuptools import setup, find_packages
+
+with open('README.md') as f:
+    readme = f.read()
+
+setup(
+    name='country-as-hegemony',
+    version='0.1.0',
+    description="Measuring AS dependency of a country",
+    long_description=readme,
+    long_description_content_type="text/markdown",
+    author='Romain Fontugne',
+    url='https://github.com/InternetHealthReport/country-as-hegemony',
+    packages=find_packages(exclude=('tests', 'docs')),
+    install_requires=[
+        'abondance',
+        'arrow',
+        'iso3166',
+        ]
+)
+
