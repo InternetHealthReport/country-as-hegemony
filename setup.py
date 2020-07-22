9a38a96cb5bdfa2681bddec1789c29ea30fec95c
romain.fontugne <romain.fontugne@gmail.com>
2020-07-22 13:01:22 +0900

    update script directory

--- setup.py
+++ setup.py
@@ -7,14 +7,14 @@ with open('README.md') as f:
 
 setup(
     name='country-as-hegemony',
-    version='0.1.0',
+    version='0.1.1',
     description="Measuring AS dependency of a country",
     long_description=readme,
     long_description_content_type="text/markdown",
     author='Romain Fontugne',
     url='https://github.com/InternetHealthReport/country-as-hegemony',
     packages=find_packages(exclude=('tests', 'docs')),
-    scripts=['country-hege'],
+    scripts=['bin/country-hege'],
     install_requires=[
         'abondance',
         'arrow',
