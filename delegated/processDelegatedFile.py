af495aaef6f9f48197bd4f9b752d6d13a53e8363
romain.fontugne <romain.fontugne@gmail.com>
2021-11-01 15:40:08 +0900

    code cleaning

--- delegated/processDelegatedFile.py
+++ delegated/processDelegatedFile.py
@@ -13,8 +13,7 @@ logging.basicConfig(
         format=FORMAT, filename='country-hege.log' , 
         level=logging.INFO, datefmt='%Y-%m-%d %H:%M:%S'
         )
-logging.info("Started: %s" % sys.argv)
-logging.info("Arguments: %s" % sys.argv)
+logging.info(f'Started: {sys.argv}')
 
 countries = []
 if len(sys.argv) > 2:
@@ -30,17 +29,18 @@ delegated_file = sys.argv[1]
 output_directory = delegated_file+'_results/'
 os.makedirs(output_directory, exist_ok=True)
 
-data = pd.read_csv(delegated_file, sep='|', header=None,  
+raw_data = pd.read_csv(delegated_file, sep='|', header=None,  
         names=['rir', 'cc', 'type', 'val0', 'val1', 'date', 'status', 'misc0', 'misc1'] )
 
+data =  raw_data[raw_data['status']=='assigned']
+
 if not countries:
-    countries = data[data['status']=='assigned']['cc'].unique()
-    logging.info('Found ', len(countries), ' countries')
+    countries = data['cc'].unique()
+    logging.info(f'Found {len(countries)} countries')
 
 # create AS files per country
 for cc in countries:
     asns = data[(data['cc']==cc) & 
-            (data['status'] == 'assigned') & 
             (data['type'] == 'asn')]['val0'].unique()
 
     filename_in = f'{output_directory}/{cc}_asns.txt'
@@ -50,7 +50,7 @@ for cc in countries:
         for asn in asns:
             fp.write(f'{asn} 1\n')
 
-    logging.info('Processing ', cc)
+    logging.info(f'Processing {cc}')
     os.system(f'python3 bin/country-hege -t 10000 -w file {filename_in} > {filename_out}_ASweights.txt')
     os.system(f'python3 bin/country-hege -t 10000 -w file -r {filename_in} > {filename_out}_ASweights_onlyTransit.txt')
     os.system(f'python3 bin/country-hege -t 10000 -m 0.01 {cc} > {filename_out}_eyeballWeights.txt')
